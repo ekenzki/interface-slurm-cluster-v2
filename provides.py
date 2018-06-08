1213843138639a596c98098ad6456605d9c1a7dd
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2018-06-08 09:44:59 +0200

    rework for v2 implementation with reactive endpoints

--- provides.py
+++ provides.py
@@ -1,111 +1,64 @@
-from collections import defaultdict
+# import charms.reactive.flags as flags
+import charms.reactive as reactive
+import socket
 
-from yaml import safe_load
-
-from charmhelpers.core.hookenv import atexit
-from charmhelpers.core.hookenv import relation_get
-from charmhelpers.core.hookenv import related_units
 from charmhelpers.contrib.templating.contexts import dict_keys_without_hyphens
+from charmhelpers.core.hookenv import (
+    DEBUG,
+    log,
+)
 
-from charms.reactive import RelationBase
-from charms.reactive import hook
-from charms.reactive import scopes
-from charms.reactive.helpers import data_changed
+class SlurmProvides(reactive.Endpoint):
+    def send_controller_config(self, config):
+        # have to handle every relation as there may be many different compute
+        # node relations
+        for rel in self.relations:
+            rel.to_publish.update(config)
 
+    def network_details(self):
+        log("network_details: to_publish: {}"
+            .format(self.relations[0].to_publish), level=DEBUG)
+        return {
+            'hostname': socket.gethostname(),
+            'ingress_address': self.relations[0].to_publish['ingress-address']
+        }
 
-class SlurmProvides(RelationBase):
-    scope = scopes.GLOBAL
+    def get_node_data(self):
+        """Return a list of dictionaries with info for each node"""
+        return self._get_all_nodes(keys=set(['ingress-address', 'hostname',
+                                         'partition', 'default']))
 
-    @hook('{provides:slurm-cluster}-relation-{joined,changed}')
-    def changed(self):
-        self._update_states()
-
-    @hook('{provides:slurm-cluster}-relation-{departed,broken}')
-    def departed(self):
-        self._update_states()
-
-    def _update_states(self):
-        if self._nodes_ready():
-            if self.get_nodes():
-                self.set_state('{relation_name}.available')
-                if data_changed('nodes', self.get_nodes()):
-                    self.set_state('{relation_name}.changed')
-                    atexit(
-                        lambda: self.remove_state('{relation_name}.changed'))
-        else:
-            self.remove_state('{relation_name}.available')
-
-    def _nodes_ready(self):
-        """Return True if all remote units have a private address."""
-        return self._get_remote_all(keys=['private-address'])
-
-    def get_nodes(self):
-        """Return a list of dictionaries with info for each node."""
-        return self._get_remote_all(
-            keys=['hostname', 'private-address', 'partition', 'default'])
-
-    def get_partitions(self):
-        """Return the partitions and their nodes as a dictionary.
-
-        :return: Dictionary with partitions as keys and list of nodes as
-            values.
-        :rtype: dict
-
-        Example::
-
-            >>> print(get_partitions())
-            {
-                'partition1': {
-                    'hosts': ['node1', 'node2', 'node3'],
-                    'default': True,
-                },
-                'partition2': {
-                    'hosts': ['node4'],
-                    'default': False,
-                }
-            }
-
-        """
-        # This could probably be done in a better way
-        part_dict = defaultdict(dict)
-        for node in self.get_nodes():
-            part_dict[node['partition']].setdefault('hosts', [])
-            part_dict[node['partition']]['hosts'].append(node['hostname'])
-            part_dict[node['partition']]['default'] = node['default']
-        return dict(part_dict)
-
-    def _get_remote_all(self, keys):
+    def _get_all_nodes(self, keys):
         """Return a list of dictionaries of values presented by remote units.
 
-        :param list keys: The list of keys to retrieve from all remote units.
+        :param set keys: A set of keys to retrieve from all remote units.
         :return: List of dictionaries with all values from all remote units.
         :rtype: list
 
         Example::
 
-            >>> print(_get_remote_all(keys=['hostname']))
+            >>> print(_get_all_nodes(keys=['hostname']))
             [
                 { 'hostname': 'host1' },
                 { 'hostname': 'host2' }
             ]
 
         """
-        values = []
-        for conversation in self.conversations():
-            for relation_id in conversation.relation_ids:
-                for unit in related_units(relation_id):
-                    remote_dict = {}
-                    for key in keys:
-                        # Values from relation_get are strings, but we want
-                        # proper Python values so use safe_load.
-                        # If relation_get returns None, yaml.safe_load will
-                        # crash so pass 'None' instead.
-                        value = relation_get(key, unit, relation_id) or 'None'
-                        remote_dict[key] = safe_load(value)
-                    values.append(dict_keys_without_hyphens(remote_dict))
-        return values
-
-    def send_controller_config(self, config):
-        self.set_remote(data={
-            'config': config,
-        })
+        res = []
+        for u in self.all_joined_units.values():
+            received = u.received
+            received_keys = set(received.keys())
+            log("Processing node unit {}, received_keys: {}"
+                .format(u.unit_name, received_keys), level=DEBUG)
+            keys_present = received_keys.issuperset(keys)
+            # skip a unit if it has not provided the necessary keys yet
+            if not keys_present:
+                log("Ignoring unit {}, received_keys: {}"
+                    .format(u.unit_name, received_keys), level=DEBUG)
+                continue
+            res.append(dict_keys_without_hyphens(
+                {k: received[k] for k in received_keys
+                 if k in keys}))
+            log("Added node unit {}, received_keys: {}"
+                .format(u.unit_name, received_keys), level=DEBUG)
+        return res
--- requires.py
+++ requires.py
@@ -1,51 +1,93 @@
-import yaml
+import charms.reactive.flags as flags
+import charms.reactive as reactive
+import charms.reactive.helpers as helpers
 
-from charmhelpers.core.hookenv import atexit
-from charmhelpers.core.hookenv import unit_private_ip
+from charmhelpers.core.hookenv import (
+    DEBUG,
+    log,
+)
 
-from charms.reactive import RelationBase
-from charms.reactive import hook
-from charms.reactive import scopes
-from charms.reactive.helpers import data_changed
+class SlurmRequires(reactive.Endpoint):
 
+    def _controller_relation(self):
+        # can only be related to a single controller
+        assert len(self.relations) < 2
+        return self.relations[0]
 
-class SlurmRequires(RelationBase):
-    scope = scopes.GLOBAL
-
-    @hook('{requires:slurm-cluster}-relation-{joined,changed}')
-    def changed(self):
-        self.set_state('{relation_name}.connected')
-        if self._controller_config_ready():
-            self.set_state('{relation_name}.available')
-            if data_changed('config', self.get_config()):
-                self.set_state('{relation_name}.changed')
-                atexit(lambda: self.remove_state('{relation_name}.changed'))
-        else:
-            self.remove_state('{relation_name}.available')
-
-    @hook('{requires:slurm-cluster}-relation-{departed,broken}')
-    def departed(self):
-        self.remove_state('{relation_name}.available')
-        self.remove_state('{relation_name}.connected')
-        self.remove_state('{relation_name}.changed')
+    @property
+    def ingress_address(self):
+        return self._controller_relation().to_publish['ingress-address']
 
     def send_node_info(self, hostname, partition, default):
-        self.set_remote(data={
+        # can only handle a single controller relation both active and
+        # standby receive the same node info for this relation
+        rel = self._controller_relation()
+        rel.to_publish.update({
             'hostname': hostname,
             'partition': partition,
             'default': default,
         })
 
-    def get_config(self):
-        """Returns the configuration from the controller as a dict."""
-        return yaml.safe_load(self.get_remote('config'))
-
-    def _controller_config_ready(self):
-        """Returns True if we find this node in the controller config."""
-        if self.get_remote('config'):
-            config = self.get_config()
+    def _controller_config_ready(self, config):
+        '''Returns True if we find this node in the controller config.
+        '''
+        if config:
+            log('Determining readiness by config: {}'.format(config))
             for node in config.get('nodes'):
-                if unit_private_ip() in node.values():
+                if node['ingress_address'] == self.ingress_address:
+                    log('The controller is ready')
                     return True
         else:
+            log('The controller is NOT ready')
             return False
+
+    @reactive.when('endpoint.{endpoint_name}.changed')
+    def controllers_changed(self):
+        """Assess active-backup controllers and set the appropriate
+        flags based on the result. Relation data for both controllers
+        is relevant as address and hostname information is taken for
+        a backup controller as well. Changed active and backup
+        .changed events are processed in properties - layers need to
+        handle active and backup config changed events in a single
+        handler"""
+        self._active_data = self._controller_config()
+
+        if self._controller_config_ready(self._active_data):
+            flags.set_flag(self.expand_name(
+                'endpoint.{endpoint_name}.active.available'))
+            flags.set_flag(self.expand_name(
+                'endpoint.{endpoint_name}.active.changed'))
+            # TODO: JSON is not serializable => need to either remove
+            # this and execute more or solve the problem
+            #if helpers.data_changed('active_data', self._active_data):
+            #    flags.set_flag(self.expand_name(
+            #        'endpoint.{endpoint_name}.active.changed'))
+
+        # processed the relation changed event - can clear this flag now
+        flags.clear_flag(self.expand_name('changed'))
+
+    @property
+    def active_data(self):
+        return self._active_data
+
+    def _controller_config(self):
+        rel = self._controller_relation()
+
+        partitions = None
+        for u in rel.joined_units:
+            recv = u.received
+            log('Received from {}: {}'.format(u.unit_name, recv))
+            # the expectation is that backup controller units will not
+            # post any config and there will only be one config posted
+            # by the active controller
+            cpartitions = recv.get('partitions')
+            if partitions and cpartitions:
+                # catch a split-brain condition when two controllers
+                # advertise possibly conflicting config data which means
+                # that active controller change 
+                # TODO: error/status handling for this
+                flags.set_flag(self.expand_name(
+                    'endpoint.{endpoint_name}.split-brain'))
+            else:
+                partitions = cpartitions
+        return recv if partitions else {}
